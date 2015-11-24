5cb68c133fd3fdbc934aadef93c77a512009ce6b
VT <vtr@gmail.com>
2015-11-24 13:10:03 +0530

    Functionality Added.

---
+++ in/tablet/Tablet.java
@@ -0,0 +1,10 @@
+package in.tablet;
+
+import in.shared.Shared;
+
+public class Tablet{
+	public static void main(String arg[]){
+		Shared shared = new Shared("Handset");		
+		System.out.println(shared);
+	}
+}
